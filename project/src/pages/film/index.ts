ac9edfbde42f681acd446b02b54dcb052f56ff79
ViktoriaMikhaylovskaya <mviktoria065@gmail.com>
2022-07-20 20:09:38 +0300

    Настроит воспроизведение видео

--- project/src/pages/film/index.ts
+++ project/src/pages/film/index.ts
@@ -1,1 +1,1 @@
-export { default as Film } from './Film';
+export { default as Film } from './film';
--- project/src/pages/index.js
+++ project/src/pages/index.js
@@ -1,7 +1,7 @@
-export { default as Film } from './film/Film';
-export { default as Main } from './main/Main';
+export { default as Film } from './film/film';
+export { default as Main } from './main/main';
 export { default as MyList } from './my-list/MyList';
 export { default as SignIn } from './sign-in/SignIn';
-export { default as Player } from './player/Player';
-export { default as Review } from './review/Review';
+export { default as Player } from './player/player';
+export { default as Review } from './review/review';
 
